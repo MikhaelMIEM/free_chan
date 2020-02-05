3c4b175d95575043afa54b4b0028b218f628e232
MikhaelMIEM <mvnikolenko@edu.hse.ru>
2020-02-06 00:05:06 +0300

    rollback (9

--- setup.py
+++
@@ -1,9 +0,0 @@
-from django.contrib.auth.models import User
-
-User.objects.create_superuser('admin', 'mikhail@example.com', 'assword')
-
-from .boards.models import Board
-
-Board.objects.create(name='b', description='Random')
-Board.objects.create(name='pony', description='My Little Pony')
-Board.objects.create(name='rns', description='Russian National State')
